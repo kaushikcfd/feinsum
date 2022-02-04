3738750649cb37c38edda5814a87634b7a75bb70
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-02-04 13:36:34 -0600

    adds device data for nvidia 1650

--- src/feinsum/data/device_info.py
+++ src/feinsum/data/device_info.py
@@ -1,8 +1,13 @@
 # Mapping from device name to peak flop rate in GFlOps/s
 DEV_TO_PEAK_GFLOPS = {"NVIDIA TITAN V": {"float32": 12288,
                                          "float64":  6144,
-                                         }
+                                         },
+                      "NVIDIA GeForce GTX 1650": {"float32": 3916.0,
+                                                  "float64":  122.4,
+                                                  },
                       }
 
 # Mapping from device name to peak bandwidth in GB/s
-DEV_TO_PEAK_BW = {"NVIDIA TITAN V": 652.8}
+DEV_TO_PEAK_BW = {"NVIDIA TITAN V":          652.8,
+                  "NVIDIA GeForce GTX 1650": 192.0,
+                  }
