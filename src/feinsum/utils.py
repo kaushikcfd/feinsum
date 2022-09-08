2ae6df6b4682432f22239dec16d00b50e9b2f4a5
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-09-08 15:54:33 -0500

    implements utils.is_any_redn_dim_parametric

--- src/feinsum/utils.py
+++ src/feinsum/utils.py
@@ -1,9 +1,10 @@
 """
 .. automethod:: has_similar_subscript
+.. automethod:: is_any_redn_dim_parametric
 """
 
 
-from feinsum.einsum import FusedEinsum
+from feinsum.einsum import FusedEinsum, SummationAxis, SizeParam
 from feinsum import array
 from feinsum.make_einsum import einsum as build_einsum
 
@@ -13,7 +14,6 @@ def has_similar_subscript(einsum: FusedEinsum,
     """
     Returns *True* only if *einsum*'s expression being applied
     to its operands is equivalently represented by *subscript*.
-
 
     .. testsetup::
 
@@ -51,3 +51,40 @@ def has_similar_subscript(einsum: FusedEinsum,
     else:
         return (ref_einsum.access_descriptors
                 == einsum.access_descriptors)
+
+
+def is_any_redn_dim_parametric(einsum: FusedEinsum) -> bool:
+    """
+    .. testsetup::
+
+        >>> from feinsum.utils import is_any_redn_dim_parametric
+        >>> import numpy as np
+
+    .. doctest::
+
+        >>> import feinsum as f
+        >>> ensm = f.einsum("ij,j->i",
+        ...                 f.array((10, 4), "float64"),
+        ...                 f.array(4, "float64"))
+        >>> is_any_redn_dim_parametric(ensm)
+        False
+        >>> ensm = f.einsum("ij,jk->i",
+        ...                 f.array((10, 4), "float64"),
+        ...                 f.array((4, 20), "float64"))
+        >>> is_any_redn_dim_parametric(ensm)
+        False
+        >>> ensm = f.einsum("i,j->i",
+        ...                 f.array(125, "float64"),
+        ...                 f.array(np.inf, "float64"))
+        >>> is_any_redn_dim_parametric(ensm)
+        True
+    """
+
+    for arg_shape, access_descrs in zip(einsum.arg_shapes,
+                                        einsum.access_descriptors):
+        for access_descr, dim in zip(access_descrs, arg_shape):
+            if (isinstance(access_descr, SummationAxis)
+                    and isinstance(dim, SizeParam)):
+                return True
+
+    return False
