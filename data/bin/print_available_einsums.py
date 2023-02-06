2bd24fb94d8ea29d5c89c8fc63475ba46b92e7b1
Kaushik Kulkarni <kgk2@illinois.edu>
2023-02-06 12:22:44 -0600

    Add helper to get high level view of the available database

---
+++ data/bin/print_available_einsums.py
@@ -0,0 +1,18 @@
+import pyopencl as cl
+import feinsum as fnsm
+
+
+def main():
+    from feinsum.sql_utils import get_timed_einsums_in_db
+    cl_ctx = cl.create_some_context()
+    cq = cl.CommandQueue(cl_ctx)
+    einsums_in_db = get_timed_einsums_in_db(cq.device)
+    print(f"Total einsums in DB = {len(einsums_in_db)}")
+
+    for i, einsum in enumerate(einsums_in_db):
+        facts = fnsm.query(einsum, cl_ctx)
+        print(f"Einsum: {einsum}, Available data: {len(facts)}")
+
+
+if __name__ == "__main__":
+    main()
