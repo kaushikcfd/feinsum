7c588df12f41648a5b7d4afd8a164e296c967a57
Kaushik Kulkarni <kaushikcfd@gmail.com>
2023-02-24 22:55:14 -0600

    some minor changes to baatched mass inverse to enhance its generality

--- src/feinsum/tuning/impls/e_ij_ej_to_ei_no_prftch.py
+++ src/feinsum/tuning/impls/e_ij_ej_to_ei_no_prftch.py
@@ -1,5 +1,6 @@
 import feinsum as fnsm
 import loopy as lp
+import loopy.match as lp_match
 import numpy as np
 
 from feinsum.tuning import IntParameter
@@ -9,16 +10,19 @@ from typing import Optional, Any
 @fnsm.tuning.einsum_arg("noutputs", lambda e: e.noutputs)
 @fnsm.tuning.einsum_arg("ndofs", lambda e: e.shape[1])
 @fnsm.tuning.transform_param(
-    "nworkitems_per_e", lambda e: IntParameter(1, e.shape[1]))
+    "nworkitems_per_e", lambda e: IntParameter(8, 8))
 @fnsm.tuning.transform_param(
-    "n_e_per_wg", lambda e: IntParameter(1, 32))
+    "n_e_per_wg", lambda e: IntParameter(4, 4))
 def transform(t_unit: lp.TranslationUnit,
               ndofs: int, noutputs: int,
               n_e_per_wg: int, nworkitems_per_e: int,
               insn_match: Optional[Any] = None,
               kernel_name: Optional[str] = None) -> lp.TranslationUnit:
+    if n_e_per_wg * nworkitems_per_e > 600:
+        raise fnsm.InvalidParameterError("Block dimension limit exceeded")
 
     kernel_name = kernel_name or t_unit.default_entrypoint.name
+    within = lp_match.parse_match(insn_match)
 
     ref_einsum = fnsm.fused_einsum("e,ij,ej->ei",
                                    [(np.inf,),
@@ -32,7 +36,7 @@ def transform(t_unit: lp.TranslationUnit,
     subst_map = fnsm.match_t_unit_to_einsum(t_unit,
                                             ref_einsum,
                                             kernel_name=kernel_name,
-                                            insn_match=insn_match)
+                                            insn_match=within)
     vng = t_unit[kernel_name].get_var_name_generator()
 
     e = subst_map["e"]
@@ -41,10 +45,9 @@ def transform(t_unit: lp.TranslationUnit,
     e_inner, e_outer = vng(f"{e}_inner"), vng(f"{e}_outer")
     i_inner, i_outer = vng(f"{i}_inner"), vng(f"{i}_outer")
     J = subst_map["J"]
-    # out1 = subst_map["_fe_out"]
-    out2 = subst_map["_fe_out_0"]
-    out3 = subst_map["_fe_out_1"]
-    out4 = subst_map["_fe_out_2"]
+    ref_outs = ["_fe_out"] + [f"_fe_out_{iout}" for iout in range(noutputs-1)]
+    outs = tuple(subst_map[ref_out] for ref_out in ref_outs)
+    del ref_outs
 
     t_unit = t_unit.with_kernel(
         lp.hoist_invariant_multiplicative_terms_in_sum_reduction(t_unit[kernel_name],
@@ -63,23 +66,16 @@ def transform(t_unit: lp.TranslationUnit,
                                                               e_inner,
                                                               i_inner]),
                            default_tag="unr",
+                           within=within,
                            temporary_address_space=lp.AddressSpace.PRIVATE)
 
-    t_unit = lp.duplicate_inames(t_unit, (i_outer, j,),
-                                 within=f"writes:{out2}",
-                                 tags={i_outer:
-                                       t_unit[kernel_name].inames[i_outer].tags,
-                                       j: t_unit[kernel_name].inames[j].tags})
-    t_unit = lp.duplicate_inames(t_unit, (i_outer, j,),
-                                 within=f"writes:{out3}",
-                                 tags={i_outer:
-                                       t_unit[kernel_name].inames[i_outer].tags,
-                                       j: t_unit[kernel_name].inames[j].tags})
-    t_unit = lp.duplicate_inames(t_unit, (i_outer, j,),
-                                 within=f"writes:{out4}",
-                                 tags={i_outer:
-                                       t_unit[kernel_name].inames[i_outer].tags,
-                                       j: t_unit[kernel_name].inames[j].tags})
+    for out in outs[1:]:
+        t_unit = lp.duplicate_inames(
+            t_unit, (i_outer, j),
+            within=lp_match.And((within,
+                                 lp_match.Writes(out))),
+            tags={i_outer: t_unit[kernel_name].inames[i_outer].tags,
+                  j: t_unit[kernel_name].inames[j].tags})
     return t_unit
 
 
@@ -87,8 +83,8 @@ if __name__ == "__main__":
     import pyopencl as cl
     import os
 
-    Ndof = 35
-    Nfields = 5
+    Ndof = 4
+    Nfields = 16
 
     cl_ctx = cl.create_some_context()
 
